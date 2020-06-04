83a687e732c86772ce35391a99553d5dde34d7d0
Carlos Vagner <47748982+glitchysnitchy@users.noreply.github.com>
2020-06-03 21:52:52 -0300

    backend form

--- js/create-point.js
+++ js/create-point.js
@@ -23,12 +23,15 @@ function populateCity(event)
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/municipios`
 
+  citySelect.innerHTML = "<option value=''>Selecione a Cidade</option>"
+  citySelect.disabled = true;
+
   fetch(url)
   .then( res => res.json() )
   .then(cities => {
     for(const city of cities)
     {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
     citySelect.disabled = false;
   })
@@ -37,3 +40,41 @@ function populateCity(event)
 document
   .querySelector('select[name=state]')
   .addEventListener("change", populateCity);
+
+const itensCollect = document.querySelectorAll('.itens-grid li');
+
+for (const item of itensCollect)
+{
+  item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector('input[name=itens]');
+
+let selectedItens = []
+
+function handleSelectedItem()
+{
+  const itemLi = event.target;
+  itemLi.classList.toggle("selected");
+  const itemId = event.target.dataset.id;
+  const alreadySelected = selectedItens.findIndex( (item) =>
+  {
+    const itemFound = item == itemId;
+    return itemFound;
+  })
+
+  if(alreadySelected >= 0)
+  {
+    const filteredItems = selectedItens.filter(item => 
+      {
+        const itemIsDifferent = item != itemId;
+        return itemIsDifferent;
+      })
+      selectedItens = filteredItems;
+  }
+  else
+  {
+    selectedItens.push(itemId);
+  }
+  collectedItems.value = selectedItens;
+}
